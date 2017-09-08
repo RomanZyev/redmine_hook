7d30507ca4062bf911614411f21bec84dede46b1
Roma <RomanZyev@gmail.com>
2017-09-08 15:40:45 +0300

    edit forms 2

--- app/controllers/text_hook_controller.rb
+++ app/controllers/text_hook_controller.rb
@@ -28,6 +28,7 @@ class TextHookController < ApplicationController
    	@txt.update_attributes(text_params)
 		if @txt.save
       		redirect_to action: :index
+      		update_main
     	else
       		render :action => 'edit'
     	end
@@ -44,11 +45,35 @@ class TextHookController < ApplicationController
 
     	redirect_to action: :index	
    	end	
-   	
+
   private
   
   def text_params
     params.require(:text_hook).permit(:txt)
   end
+
+  def update_main
+    id = params[:id]
+    @new_list = params[:listhook_id]
+    @old_list = SetingText.where("text_hook_id" =>id).pluck("list_hook_id")
+
+    if @new_list != nil
+      @new_list.each do |l|
+        if !@old_list.include?(l.to_i)
+          SetingText.new("text_hook_id" =>id,"list_hook_id" =>l.to_i).save
+        end
+      end
+      @old_list.each do |l|
+        if !@new_list.include?(l.to_s) && l != nil
+          SetingText.where("text_hook_id" =>id,"list_hook_id" =>l).first.destroy
+        end
+      end
+    else
+      @old_list.each do |l|
+        SetingText.where("text_hook_id" =>id,"list_hook_id" =>l).first.destroy
+      end
+    end   
+   
+  end   
 
 end
